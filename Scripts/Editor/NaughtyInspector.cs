15a9509ab47fc055a11dad1481d1a497ff81afff
adamgryu <adamgryu@gmail.com>
2026-03-05 13:51:02 -0500

    added naughtyscriptableobj

--- Scripts/Editor/NaughtyInspector.cs
+++ Scripts/Editor/NaughtyInspector.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 namespace NaughtyAttributes.Editor
 {
     [CanEditMultipleObjects]
+    [CustomEditor(typeof(NaughtyScriptableObject), true)]
     [CustomEditor(typeof(NaughtyMonoBehaviour), true)]
     public class NaughtyInspector : UnityEditor.Editor
     {
